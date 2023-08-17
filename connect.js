ae406e8186c4903b44582060527ba0601c51d3a3
sara-rioseco <sara.rioseco@gmail.com>
2023-08-17 17:18:33 -0400

    trying to fix config

--- connect.js
+++ connect.js
@@ -1,10 +1,16 @@
+const { MongoClient } = require('mongodb');
 const config = require('./config');
 
-// eslint-disable-next-line no-unused-vars
-const { dbUrl } = config;
+const client = new MongoClient(config.dbUrl);
 
 async function connect() {
-  // TODO: Conexión a la Base de Datos
+  try {
+    await client.connect();
+    const db = client.db('db');
+    return db;
+  } catch (error) {
+    //
+  }
 }
 
 module.exports = { connect };
