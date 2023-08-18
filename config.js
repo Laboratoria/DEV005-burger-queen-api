4a72978e8b4f2cfc8f350e1ce9d84bbf7749cbe1
sara-rioseco <sara.rioseco@gmail.com>
2023-08-18 11:49:21 -0400

    initial config docker with node and mongodb

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://bq:secret@localhost:27017/'; // ojo con esto
+exports.dbUrl = process.env.MONGO_URL
+  || process.env.DB_URL
+  || 'mongodb://bq:secret@localhost:27017/';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
