1676f7e195274f8b4590fcf035e4ea644231ecb9
sara-rioseco <sara.rioseco@gmail.com>
2023-08-29 23:55:43 -0400

    fixed problem with npm

--- controller/users.js
+++ controller/users.js
@@ -1,15 +1,14 @@
 const User = require('../models/user');
 
 module.exports = {
-  validateEmail : (email) => {
-    const validRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  validateEmail: (email) => {
+    const validRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return validRegEx.test(email ?? '');
   },
   getUsers: async (req, res, next) => {
     const users = await User.find()
-        .select("-password -__v")
-        .lean();
+      .select('-password -__v')
+      .lean();
     res.json(users);
   },
 };
-
