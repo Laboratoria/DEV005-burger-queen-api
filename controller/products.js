94cedc72834b9a64a5e5bcbab9dcfe3474c77e91
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-09-05 09:33:21 -0600

    add pages GET products

--- controller/products.js
+++ controller/products.js
@@ -5,18 +5,29 @@ const config = require('../config');
 const { dbUrl } = config;
 const client = new MongoClient(dbUrl);
 
-module.exports ={
+module.exports = {
     getProducts: async (req, res, next) => {
         try {
             const products = await Product.find(); // devuelve lista de productos
             console.log(products, 'product CONTROOOOOLLLLER');
 
-            res.json(products);
+            // Si req incluye paginación 
+            if (req.query.page && req.query.limit) {
+                const page = parseInt(req.query.page);
+                const limit = parseInt(req.query.limit);
+                const startIndex = (page - 1) * limit;
+                const endIndex = page * limit
+                const paginatedProducts = products.slice(startIndex, endIndex);
+                console.log('aquííííí', req.query)
+                res.status(200).json(paginatedProducts)
+            } else {
+                res.status(200).json(products)
+            }
         } catch (err) {
-          console.log("error al mostrar productos", err);
-          next(err);
+            console.log("error al mostrar productos", err);
+            next(err);
         } finally {
-          client.close();
+            client.close();
         }
-      },
+    },
 };
