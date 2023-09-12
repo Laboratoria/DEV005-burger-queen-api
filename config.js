d61f6d01162fee63608afd244e6fcb4f6f46ffc0
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-09-12 15:20:40 -0600

    try deploy

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
 exports.dbUrl = process.env.MONGO_URL
   || process.env.DB_URL
-  || 'mongodb://127.0.0.1:27017/test';
+  || 'mongodb+srv://BQAKarlaSara:BQAKarlaSaraDev007@bqa.px2rco8.mongodb.net/?retryWrites=true&w=majority';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@prueba.com';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
--- connect.js
+++ connect.js
@@ -1,11 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const { MongoClient } = require('mongodb');
 const config = require('./config');
+require('dotenv').config();
 
-const client = new MongoClient(config.dbUrl);
+const client = new MongoClient(process.env.MONGO_URL);
 
 async function connect() {
   try {
     await client.connect();
+    console.log(process.env.MONGO_URL, 'connect heeeeere');
     const db = client.db();
     return db;
   } catch (error) {
