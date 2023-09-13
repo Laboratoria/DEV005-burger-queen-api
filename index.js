d954739146727819c79fc1f14c64de6ee6b399d4
sara-rioseco <sara.rioseco@gmail.com>
2023-09-13 14:08:00 -0300

    adds cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const config = require('./config');
 const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
@@ -14,6 +15,10 @@ app.set('pkg', pkg);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(authMiddleware(secret));
+app.use(cors({
+  origin: ['https://burger-queen-api-wp1d-dev.fl0.io/'],
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+}));
 
 // Registrar rutas
 routes(app, (err) => {
